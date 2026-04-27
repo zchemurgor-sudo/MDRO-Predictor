0e88db30eee665e9719eb0f0f9928c7129f44584
zuenah <zchemurgor@gmail.com>
2026-04-27 15:54:45 +0300

    Create modeling.py

---
+++ modeling.py
@@ -0,0 +1,8 @@
+# MDRO Prediction Model - Initial Script
+
+def main():
+    print("Loading MDRO Clinical Specification...")
+    # On Thursday, we will add the Scikit-Learn code here!
+
+if __name__ == "__main__":
+    main()
